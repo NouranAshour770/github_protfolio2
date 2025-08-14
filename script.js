1864a9c9b7354546dfc3c878e5379a85b6d5b5b5
NouranAshour770 <nouranashourr@gmail.com>
2025-08-14 14:00:13 +0300

    intial

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 // AOS animations
+// AOS animations
 AOS.init({ duration: 800, once: true });
+
+// إعادة تهيئة AOS بعد تحميل كل العناصر
+window.addEventListener('load', function () {
+    AOS.refresh();
+});
+
